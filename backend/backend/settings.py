55b6e1faa0e2d0270c4bada568f28b25081b50c9
Sergey <s3rzhik23@yandex.ru>
2026-05-17 19:30:46 +0300

    docker new

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 
+import os
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -7,7 +10,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 's3r4.myvnc.com', '81.26.187.96']
 
 
 # Application definition
@@ -61,8 +64,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+	'USER': os.getenv('POSTGRES_USER', 'django'),
+	'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+	'HOST': os.getenv('DB_HOST', ''),
+	'PORT': os.getenv('DB_PORT', 5432)
     }
 }
 
@@ -105,6 +112,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = BASE_DIR / 'collected_static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
